0cbb0bcaabc372c391331ec6c4380752b0f535b9
chadunnett <chadunnett@gmail.com>
2022-11-16 16:44:15 -0600

    commit

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (name)
+VALUES ("The Great Gatsby"),
+       ("Paid In Full"),
+       ("Pineapple Express"),
+       ("Friday"),
+       ("Breakfast at Tiffanys");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ()
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ()
